367aae556dc9661c090c55be4899c9c11a03fb7d
dongsun1 <le12352@naver.com>
2022-04-25 16:31:56 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const SocketIO = require("socket.io");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+const requestMiddleware = (req, res, next) => {
+  console.log(
+    "[Ip address]:",
+    req.ip,
+    "[method]:",
+    req.method,
+    "Request URL:",
+    req.originalUrl,
+    " - ",
+    new Date()
+  );
+  next();
+};
+
+// 각종 미들웨어
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+app.use(requestMiddleware);
+app.use(express.urlencoded({ extended: false }));
+
+const httpServer = http.createServer(app);
+const io = SocketIO(httpServer);
+
+io.on("connection", (socket) => {
+  console.log("connection: ", socket.id);
+  socket.on("msg", (msg) => {
+    console.log(msg);
+  });
+});
+
+// 서버 열기
+app.listen(port, () => {
+  console.log(port, "포트로 서버가 켜졌어요!");
+});
