df27ab08471d8a9034a90ced53972e843d114a15
dongsun1 <le12352@naver.com>
2022-04-26 13:06:51 +0900

    createRoom

--- app.js
+++ app.js
@@ -32,26 +32,46 @@ app.use(express.urlencoded({ extended: false }));
 const httpServer = http.createServer(app);
 const io = SocketIO(httpServer, { cors: { origin: "*" } });
 
-let rooms = ["room 1", "room 2"];
+let rooms = [];
 
 io.on("connection", (socket) => {
   console.log("connection: ", socket.id);
 
   io.emit("roomList", rooms);
 
+  socket.on("main", (id) => {
+    console.log(`아이디 받아오기: ${id}`);
+    socket.userId = id;
+  });
+
   socket.on("msg", (msg, id) => {
     console.log(`msg: ${msg}, id: ${id}`);
-    socket.userId = id;
-    io.to(rooms[socket.roomNum]).emit("msg", { msg, id });
+    io.to(socket.roomId).emit("msg", { msg, id });
   });
 
-  socket.on("joinRoom", (roomNum) => {
-    socket.join(rooms[roomNum]);
-    socket.roomNum = roomNum;
+  socket.on("joinRoom", (socketId) => {
+    console.log(`${socket.userId}님이 입장하셨습니다.`);
+    for (let i = 0; i < rooms.length; i++) {
+      if (rooms[i].socketId === socketId) {
+        socket.join(rooms[i].socketId);
+        socket.roomId = rooms[i].socketId;
+        break;
+      }
+    }
   });
 
-  socket.on("createRoom", (room) => {
+  socket.on("createRoom", (userId, roomTitle, roomPeople, password) => {
+    const socketId = socket.id;
+    const room = {
+      socketId,
+      userId,
+      roomTitle,
+      roomPeople,
+      password,
+    };
     rooms.push(room);
+    console.log(`방 만들기: ${room}`);
+    socket.emit("roomData", room);
   });
 
   socket.on("disconnect", function () {
