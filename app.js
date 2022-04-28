ac6d11601e999f35a90d7a11efa6b86e854ab2b9
dongsun1 <le12352@naver.com>
2022-04-28 14:40:31 +0900

    https 에러 수정

--- app.js
+++ app.js
@@ -24,15 +24,15 @@ const requestMiddleware = (req, res, next) => {
   next();
 };
 
-// app.use((req, res, next) => {
-//   if (req.secure) {
-//     next();
-//   } else {
-//     const to = `https://${req.hostname}:${httpsPort}${req.url}`;
-//     console.log(to);
-//     res.redirect(to);
-//   }
-// });
+app.use((req, res, next) => {
+  if (req.secure) {
+    next();
+  } else {
+    const to = `https://${req.hostname}:${httpsPort}${req.url}`;
+    console.log(to);
+    res.redirect(to);
+  }
+});
 
 // 각종 미들웨어
 app.use(cors());
@@ -42,27 +42,27 @@ app.use(cookieParser());
 app.use(requestMiddleware);
 app.use(express.urlencoded({ extended: false }));
 
-// app.get(
-//   "/.well-known/pki-validation/C30850814E6E08C0AEFA95972F1708D6.txt",
-//   (req, res) => {
-//     res.sendFile(
-//       __dirname +
-//         "/well-known/pki-validation/C30850814E6E08C0AEFA95972F1708D6.txt"
-//     );
-//   }
-// );
+app.get(
+  "/.well-known/pki-validation/C30850814E6E08C0AEFA95972F1708D6.txt",
+  (req, res) => {
+    res.sendFile(
+      __dirname +
+        "/well-known/pki-validation/C30850814E6E08C0AEFA95972F1708D6.txt"
+    );
+  }
+);
 
-// const privateKey = fs.readFileSync(__dirname + "/private.key", "utf8");
-// const certificate = fs.readFileSync(__dirname + "/certificate.crt", "utf8");
-// const ca = fs.readFileSync(__dirname + "/ca_bundle.crt", "utf8");
-// const credentials = {
-//   key: privateKey,
-//   cert: certificate,
-//   ca: ca,
-// };
+const privateKey = fs.readFileSync(__dirname + "/private.key", "utf8");
+const certificate = fs.readFileSync(__dirname + "/certificate.crt", "utf8");
+const ca = fs.readFileSync(__dirname + "/ca_bundle.crt", "utf8");
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
 
 const httpServer = http.createServer(app);
-// const httpsServer = https.createServer(credentials, appH);
+const httpsServer = https.createServer(credentials, appH);
 
 const io = SocketIO(httpServer, { cors: { origin: "*" } });
 
@@ -162,10 +162,10 @@ io.on("connection", (socket) => {
 });
 
 // 서버 열기
-httpServer.listen(3000, () => {
+httpServer.listen(httpPort, () => {
   console.log(httpPort, "포트로 서버가 켜졌어요!");
 });
 
-// httpsServer.listen(httpsPort, () => {
-//   console.log(httpsPort, "포트로 서버가 켜졌어요!");
-// });
+httpsServer.listen(httpsPort, () => {
+  console.log(httpsPort, "포트로 서버가 켜졌어요!");
+});
