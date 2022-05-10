72bcf8a21ca815c1b40319557117cb3a3c9c38ae
dongsun1 <le12352@naver.com>
2022-05-10 13:27:52 +0900

    readyPeople 주석 해제

--- utils/socket.js
+++ utils/socket.js
@@ -154,7 +154,7 @@ module.exports = (server) => {
       }
 
       const readyPeople = await Room.findOne({ roomId });
-      // io.to(roomId).emit("readyPeople", readyPeople.currentReadyPeople);
+      io.to(roomId).emit("readyPeople", readyPeople.currentReadyPeople);
     });
 
     // 게임시작
