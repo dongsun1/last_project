2bf43f82c1ddd0e978113ab5a3813351be20d14e
HYUNSEUNG91 <lhs91001@naver.com>
2022-05-11 13:55:39 +0900

    social Loing test 삭제

--- routers/user/googleLogin.js
+++ routers/user/googleLogin.js
@@ -9,9 +9,11 @@ const jwt = require("jsonwebtoken");
 // GOOGLE_CLIENTID : '106975674237-i62c0d3iv72p088027oa1vunqbsk4490.apps.googleusercontent.com'
 // GOOGLE_SECRET_KEY : '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'
 
+
 router.get('/googleLogin', (req, res) => {
-    console.log('googleLogin Router');
-    
+    // console.log('googleLogin Router');
+    // const GOOGLE_LOGIN_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENTID}&redirect_uri=https://localhost/googleLogin/main&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email`;
+    // res.redirect(GOOGLE_LOGIN_URL)
 
 })
 
