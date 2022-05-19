2e84c57cefaee0882710d3527629adc58a7683c8
dongsun1 <le12352@gmail.com>
2022-05-19 16:10:37 +0900

    프로필 에러 수정

--- routers/user/profile.js
+++ routers/user/profile.js
@@ -9,7 +9,7 @@ router.get("/profile", authMiddleWare, async (req, res) => {
     const { userId } = res.locals.user;
 
     const user = await User.findOne({ userId });
-    res.status(200).send({ result: true, profile: user.profile });
+    res.status(200).send({ result: true, profile: user.userProfile });
   } catch (e) {
     res.status(400).send({ result: false, msg: "실패" });
     console.log("profile.js에서 에러남");
@@ -22,7 +22,7 @@ router.post("/profile", authMiddleWare, async (req, res) => {
     const { userId } = res.locals.user;
     const { profile } = req.body;
 
-    await User.updateOne({ userId }, { $set: { profile } });
+    await User.updateOne({ userId }, { $set: { userProfile: profile } });
     res.status(200).send({ result: true, msg: "등록 성공" });
   } catch (error) {
     console.log(error);
