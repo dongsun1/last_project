7f3a05dc1b68259a1ae0a5e18a313a1ef90988ca
dongsun1 <le12352@gmail.com>
2022-05-20 14:09:16 +0900

    전적 get으로 변경

--- routers/user/gameRecord.js
+++ routers/user/gameRecord.js
@@ -3,22 +3,21 @@ const router = express.Router();
 const User = require("../../schemas/user/user");
 const authMiddleware = require("../../middleware/authMiddleWare");
 
-router.post("/gameRecord", authMiddleware, async (req, res) => {
-    
-    const { user } = res.locals;
-    const userId = user[0].userId;
-    const userNick = user[0].userNick
-    console.log('loginUserId :', userId, 'loginUserNick :',userNick);
+router.get("/gameRecord", authMiddleware, async (req, res) => {
+  const { user } = res.locals;
+  const userId = user[0].userId;
+  const userNick = user[0].userNick;
+  console.log("loginUserId :", userId, "loginUserNick :", userNick);
 
-    const userRecord = await User.findOne({
-        userId:userId
-    });
-    console.log('result',userRecord)
-    res.status(200).send({
-      msg: "게임전적 조회 완료.",
-      userWin : userRecord.userWin,
-      userLose : userRecord.userLose
-    });
+  const userRecord = await User.findOne({
+    userId: userId,
+  });
+  console.log("result", userRecord);
+  res.status(200).send({
+    msg: "게임전적 조회 완료.",
+    userWin: userRecord.userWin,
+    userLose: userRecord.userLose,
+  });
 });
 
 module.exports = router;
