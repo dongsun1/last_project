928cf39830498f6108367aaf55e2c6eeafe6ab33
dongsun1 <le12352@gmail.com>
2022-05-27 19:34:22 +0900

    네이버 로그인

--- routers/user/naverLogin.js
+++ routers/user/naverLogin.js
@@ -8,7 +8,7 @@ const { request } = require("http");
 
 const naver = {
   clientid: `${process.env.CLIENT_ID}`, //REST API
-  redirectUri: "https://mafiyang.com/naverLogin/main ",
+  redirectUri: "https://mafiyang.com/naverLogin/main",
   client_secret: `${process.env.CLIENT_SECRET}`,
   state: "login",
 };
