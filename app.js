571fa5dd52eee503a7a3f9905094ac0c2a1bd5e9
dongsun1 <le12352@gmail.com>
2022-06-30 23:54:04 +0900

    https

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ const app = express();
 const httpPort = 80;
 const httpsPort = 443;
 
-// const privateKey = fs.readFileSync(__dirname + "/private.key", "utf8");
-// const certificate = fs.readFileSync(__dirname + "/certificate.crt", "utf8");
-// const ca = fs.readFileSync(__dirname + "/ca_bundle.crt", "utf8");
-// const credentials = {
-//   key: privateKey,
-//   cert: certificate,
-//   ca: ca,
-// };
+const privateKey = fs.readFileSync(__dirname + "/private.key", "utf8");
+const certificate = fs.readFileSync(__dirname + "/certificate.crt", "utf8");
+const ca = fs.readFileSync(__dirname + "/ca_bundle.crt", "utf8");
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
 
 connect();
 
